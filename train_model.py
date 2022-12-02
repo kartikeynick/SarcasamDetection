fe198a21be2a25375cd115c2ec1aea6cab36b1da
Anthony <appietro516@email.arizona.edu>
2022-12-01 17:52:05 -0700

    basic train working

--- train_model.py
+++ train_model.py
@@ -3,13 +3,62 @@ from datasets import load_dataset
 from transformers import RobertaTokenizerFast, RobertaForSequenceClassification,Trainer, TrainingArguments
 import pandas as pd
 
-csv = pd.read_csv("dataset/train.csv")
-csv = csv.head(100)
-csv.to_csv('dataset/training_small.csv')
-print(csv)
+import torch
 
-# dataset = load_dataset('csv', data_files='dataset/training.csv')
-# print(dataset)
+from transformers import DistilBertTokenizerFast
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
+
+def read_train_data(path):
+    data = pd.read_csv(path)
+    texts = data['comment'].tolist()
+    labels = data['label'].tolist()
+
+    return texts, labels
+
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_texts, train_labels = read_train_data('dataset/training_small.csv')
+
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+train_dataset = MyDataset(train_encodings, train_labels)
+
+from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
+
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=3,              # total number of training epochs
+    per_device_train_batch_size=16,  # batch size per device during training
+    per_device_eval_batch_size=64,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=10,
+)
+
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+
+trainer = Trainer(
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset,         # training dataset
+    # eval_dataset=val_dataset             # evaluation dataset
+)
+trainer.train()
+
+# dataset = load_dataset('csv', data_files='dataset/training_small.csv')
+# print(dataset['train'])
 
 
 # model = RobertaForSequenceClassification.from_pretrained('roberta-base')
