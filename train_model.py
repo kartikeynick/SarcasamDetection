390b0139ad57dac21661560315cbd577cfeabf7d
Anthony <appietro516@email.arizona.edu>
2022-12-03 14:25:24 -0700

    metrics and small modeL

--- train_model.py
+++ train_model.py
@@ -1,20 +1,38 @@
 from datasets import load_dataset
 
-from transformers import RobertaTokenizerFast, RobertaForSequenceClassification,Trainer, TrainingArguments
+from transformers import (
+    RobertaTokenizerFast,
+    RobertaForSequenceClassification,
+    Trainer,
+    TrainingArguments,
+)
 import pandas as pd
+import shutil
 
 import torch
-
+import numpy as np
+import evaluate
 from transformers import DistilBertTokenizerFast
-tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
+tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
+
+SKIP_TRAIN = False  # WARNING: setting to false will destroy contents of "my_model"
+SKIP_TEST = False
+
+TRAIN_CAP = 500
+TEST_CAP = 100
 
 
-def read_train_data(path):
-    data = pd.read_csv(path).dropna().head(100)
-    texts = data['comment'].tolist()
-    labels = data['label'].tolist()
+def read_train_data(path, cap=False):
+    if type(cap) == int:
+        data = pd.read_csv(path).dropna().head(cap)
+    else:
+        data = pd.read_csv(path).dropna()
+    texts = data["comment"].tolist()
+    labels = data["label"].tolist()
 
     return texts, labels
+
 
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels):
@@ -23,14 +41,15 @@ class MyDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
+        item["labels"] = torch.tensor(self.labels[idx])
         return item
 
     def __len__(self):
         return len(self.labels)
 
-train_texts, train_labels = read_train_data('dataset/training_small.csv')
-val_texts, val_labels = read_train_data('dataset/validation.csv')
+
+train_texts, train_labels = read_train_data("dataset/train.csv", 400)
+val_texts, val_labels = read_train_data("dataset/validation.csv", 100)
 
 
 train_encodings = tokenizer(train_texts, truncation=True, padding=True)
@@ -40,30 +59,51 @@ val_dataset = MyDataset(val_encodings, val_labels)
 
 from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
 
-training_args = TrainingArguments(
-    output_dir='./results',          # output directory
-    num_train_epochs=3,              # total number of training epochs
-    per_device_train_batch_size=16,  # batch size per device during training
-    per_device_eval_batch_size=64,   # batch size for evaluation
-    warmup_steps=500,                # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # strength of weight decay
-    logging_dir='./logs',            # directory for storing logs
-    logging_steps=10,
-)
 
-model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+if not SKIP_TRAIN:
+    training_args = TrainingArguments(
+        output_dir="./results",  # output directory
+        num_train_epochs=3,  # total number of training epochs
+        per_device_train_batch_size=16,  # batch size per device during training
+        per_device_eval_batch_size=64,  # batch size for evaluation
+        warmup_steps=500,  # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,  # strength of weight decay
+        logging_dir="./logs",  # directory for storing logs
+        logging_steps=10,
+    )
 
+    model = DistilBertForSequenceClassification.from_pretrained(
+        "distilbert-base-uncased"
+    )
 
-trainer = Trainer(
-    model=model,                         # the instantiated 🤗 Transformers model to be trained
-    args=training_args,                  # training arguments, defined above
-    train_dataset=train_dataset,         # training dataset
-    eval_dataset=val_dataset             # evaluation dataset
-)
+    trainer = Trainer(
+        model=model,  # the instantiated 🤗 Transformers model to be trained
+        args=training_args,  # training arguments, defined above
+        train_dataset=train_dataset,  # training dataset
+        eval_dataset=val_dataset,  # evaluation dataset
+    )
 
-trainer.train()
-res = trainer.predict(val_dataset)
-print(res)
+    trainer.train()
+    shutil.rmtree("my_model")
+    trainer.save_model("my_model")
+if not SKIP_TEST:
+    model = DistilBertForSequenceClassification.from_pretrained("my_model")
+    trainer = Trainer(
+        model=model,  # the instantiated 🤗 Transformers model to be trained
+        train_dataset=train_dataset,  # training dataset
+        eval_dataset=val_dataset,  # evaluation dataset
+    )
+
+    preds_logits, labels, metrics = trainer.predict(val_dataset)
+    preds = np.argmax(preds_logits, axis=-1)
+    print(preds)
+
+    metrics = ["accuracy", "precision", "recall", "f1"]
+
+    for m in metrics:
+        metric = evaluate.load(m)
+        res = metric.compute(predictions=preds, references=labels)
+        print(res)
 
 
 # dataset = load_dataset('csv', data_files='dataset/training_small.csv')
